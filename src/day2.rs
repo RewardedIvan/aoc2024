b1b6fa08c15d9432664ce7ccf61e0db3d1460cab
int4_t <rewardedivan@gmail.com>
2024-12-03 01:25:28 +0200

    day2: compute while reading input

--- src/day2.rs
+++ src/day2.rs
@@ -1,7 +1,8 @@
 use std::io;
 
 pub fn day2() {
-    let mut reports: Vec<Vec<i32>> = Vec::new();
+    let mut safe_reports = 0;
+    let mut reports = 0;
 
     println!("Enter levels seperated by spaces, to exit enter END");
     loop {
@@ -18,24 +19,17 @@ pub fn day2() {
             panic!("there should be atleast 2 levels in a report");
         }
 
-        let mut levels = Vec::new();
-        for e in split {
-            levels.push(e.parse::<i32>().expect("not a valid number"));
-        }
-
-        reports.push(levels);
-    }
-
-    let mut safe_reports = 0;
-    for r in reports.iter() {
         let mut is_unsafe = false;
-        // increase = a - b < 0
-        let increase = r[0] - r[1] < 0;
-
-        for i in 1..r.len() {
-            let a = r[i - 1];
-            let b = r[i];
+        let mut increase = true;
+        for i in 1..split.len() {
+            let a = split[i - 1].parse::<i32>().expect("not a valid number");
+            let b = split[i].parse::<i32>().expect("not a valid number");
             let diff = a - b;
+
+            if i == 1 {
+                // increase = a - b < 0
+                increase = diff < 0;
+            }
 
             //println!("diff between {a} and {b} is {diff}");
             if diff.abs() > 3 || diff == 0 || increase != (diff < 0) {
@@ -45,10 +39,12 @@ pub fn day2() {
         }
 
         //println!("report deemed unsafe: {is_unsafe}");
+        reports += 1;
+
         if !is_unsafe {
             safe_reports += 1;
         }
     }
 
-    println!("safe reports: {}, unsafe reports: {}", safe_reports, reports.len() - safe_reports);
+    println!("safe reports: {}, unsafe reports: {}, total: {}", safe_reports, reports - safe_reports, reports);
 }
