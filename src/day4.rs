deea0aa10ce39a14f0debd6429091485c69c9cae
int4_t <rewardedivan@gmail.com>
2024-12-04 22:02:43 +0200

    day4: add part two

--- src/day4.rs
+++ src/day4.rs
@@ -21,19 +21,16 @@ pub fn day4() {
         ).collect();
 
     let mut appearances = 0;
+    let mut appearances_x = 0;
 
     let lines_len = lines.len();
     for y in 0..lines_len {
         let line_len = lines[y].len();
         for x in 0..line_len {
-            if lines[y][x] != 'X' && lines[y][x] != 'S' {
-                continue;
-            }
-
             let x_right_space = line_len - x - 1;
             let x_left_space = x;
             let y_down_space = lines_len - y - 1;
-            //let y_up_space = y + 1;
+            let y_up_space = y;
 
             // horizontal
             if x_right_space >= 3 && (
@@ -66,8 +63,22 @@ pub fn day4() {
                 appearances += 1;
                 //println!("left diagonal check at {},{}: {}{}{}{}", x, y, lines[y][x], lines[y+1][x-1], lines[y+2][x-2], lines[y+3][x-3]);
             }
+
+            if x_left_space >= 1 && y_down_space >= 1 && x_right_space >= 1 && y_up_space >= 1 && lines[y][x] == 'A' && (
+                // M..    S..
+                // .A. or .A.
+                // ..S    ..M
+                ((lines[y+1][x-1] == 'M' && lines[y-1][x+1] == 'S') || (lines[y+1][x-1] == 'S' && lines[y-1][x+1] == 'M')) &&
+                // ..M    ..S
+                // .A. or .A.
+                // S..    M..
+                ((lines[y+1][x+1] == 'M' && lines[y-1][x-1] == 'S') || (lines[y+1][x+1] == 'S' && lines[y-1][x-1] == 'M'))
+            ) {
+                appearances_x += 1;
+            }
         }
     }
 
     println!("appearances: {}", appearances);
+    println!("MAS as an X shape: {}", appearances_x);
 }
